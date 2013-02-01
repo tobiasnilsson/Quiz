01b416166bfb80474d443a9b2ecdde90cf017480
Tobias Nilsson <tobnil@kth.se>
2013-02-01 13:33:52 +0100

    changed db

--- Quiz/Filters/InitializeSimpleMembershipAttribute.cs
+++ Quiz/Filters/InitializeSimpleMembershipAttribute.cs
@@ -38,7 +38,7 @@ namespace Quiz.Filters
                         }
                     }
 
-                    WebSecurity.InitializeDatabaseConnection("DefaultConnection", "UserProfile", "UserId", "UserName", autoCreateTables: true);
+                    WebSecurity.InitializeDatabaseConnection("UserDB", "UserProfile", "UserId", "UserName", autoCreateTables: true);
                 }
                 catch (Exception ex)
                 {
